5f1ac3e4c8e555a927c3a09467fbbba185796198
Mikian Musser <Bobar312@gmail.com>
2019-10-27 22:36:44 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,13 +4,14 @@
 let dots = []
 let lines = []
 let windmill;
-let numDots = 15;
+let numDots = 25;
 let width = 500;
 let height = 500;
 let tolerance = 5
-let speed = 0.005
+let speed = 0.01
 
 function Dot(_x, _y) {
+  this.count = 0;
   this.x = _x;
   this.y = _y;
   this.on = false;
@@ -18,9 +19,9 @@ function Dot(_x, _y) {
   this.show = function() {
     noStroke()
     if (this.on) {
-      fill(100, 255, 100)
+      fill(100, 255, 100, 255 - this.count)
     } else {
-      fill(255)
+      fill(255, 255, 255, 255 - this.count)
     }
     ellipse(this.x, this.y, 20)
   }
@@ -41,7 +42,7 @@ function Dot(_x, _y) {
   }
 
   this.update = function(smallLine) {
-    if (!this.on) {
+    if (!this.on && !this.removed) {
       let x1 = floor(smallLine.x1);
       let y1 = floor(smallLine.y1);
       let x2 = floor(smallLine.x2);
@@ -60,6 +61,11 @@ function Dot(_x, _y) {
         this.on = true;
         windmill.cx = this.x
         windmill.cy = this.y
+        this.count = this.count + 15
+        if (this.count > 250) {
+          this.removed = true
+        }
+        // speed = speed * -1
         lines.push({
           x: windmill.cx,
           y: windmill.cy,
@@ -96,6 +102,7 @@ function Windmill(_x, _y) {
     this.y2 = this.cy + this.l * sin(this.r)
   }
   this.rotate = function() {
+    this.turn = speed
     this.r = this.r + this.turn;
     this.update();
   }
@@ -141,6 +148,7 @@ function setup() {
   }
   dots[picked].found = true;
   dots[picked].on = true;
+  dots[picked].count = 1;
   windmill = new Windmill(dots[picked].x, dots[picked].y);
 }
 
@@ -154,9 +162,9 @@ function draw() {
   }
   for (var i = 0; i < dots.length; i++) {
     dots[i].checkoff(windmill.smallLine());
-    if (dots[i].found) {
-      dots[i].show();
-    }
+    // if (dots[i].found) {
+    dots[i].show();
+    // }
   }
   windmill.show();
   for (var i = 0; i < lines.length; i++) {
